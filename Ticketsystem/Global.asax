31ffa24a4e17267e05391183eea862826b164bb8
Nero <Nero@NERO>
2017-04-22 10:14:40 +0200

    Projektdateien hinzufügen.

---
+++ Ticketsystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ticketsystem.MvcApplication" Language="C#" %>
---
+++ Ticketsystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ticketsystem.Startup))]
+namespace Ticketsystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
